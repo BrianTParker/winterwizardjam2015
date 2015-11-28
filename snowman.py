7b7c5498199a7e214ffcf6770f814de0cf6a94bf
eljefeasu <jefferson.neel@gmail.com>
2015-11-28 18:38:43 -0500

    add image to screen
    slightly altered snowman image and added new. changed color of
    background and added snowman image. I got an error when trying to add
    image with images/snowman.png path, it needed full path to work. still
    need to figure that out.

--- snowman.py
+++ snowman.py
@@ -1,10 +1,16 @@
 import pygame
 
+#add variable named BLUE and set to color used for background
+BLUE = (81, 95, 255)
+
 pygame.init()
 print("hello")
 game_display = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption('Winter Wizard Jam')
+
+#load snowman image. I was unable to figure out how to load without full path. error when /images/snowman.png
+snowman=pygame.image.load("C:/winterwizardjam2015/winterwizardjam2015/images/snowman.png").convert_alpha()
 
 fps = 60
 clock = pygame.time.Clock()
@@ -16,6 +22,10 @@ while play:
         if event.type == pygame.QUIT:
             play = False
 
+    game_display.fill(BLUE)
+
+    #display image at the bottom in the middle of the screen
+    game_display.blit(snowman, (250,300))
 
     pygame.display.update()
     clock.tick(fps)
