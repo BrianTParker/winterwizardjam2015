e710e64eb334e440d936220745ed45f429b395bd
eljefeasu <jefferson.neel@gmail.com>
2015-11-28 22:14:36 -0500

    move snowman right and left

--- snowman.py
+++ snowman.py
@@ -11,6 +11,8 @@ pygame.display.set_caption('Winter Wizard Jam')
 
 #load snowman image. I was unable to figure out how to load without full path. error when /images/snowman.png
 snowman=pygame.image.load("images/snowman.png").convert_alpha()
+x = 250
+y = 300
 
 fps = 60
 clock = pygame.time.Clock()
@@ -22,10 +24,18 @@ while play:
         if event.type == pygame.QUIT:
             play = False
 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                pressed_left = True
+                x -= 10
+            if event.key == pygame.K_RIGHT:
+                pressed_right = True
+                x += 10
+
     game_display.fill(BLUE)
 
     #display image at the bottom in the middle of the screen
-    game_display.blit(snowman, (250,300))
+    game_display.blit(snowman, (x,y))
 
     pygame.display.update()
     clock.tick(fps)
