75ddfb7fe0492763f004a5ca90007e86d67fc7ff
Brian Parker <brithom.parker@gmail.com>
2015-11-28 22:05:26 -0500

    created player class

--- snowman.py
+++ snowman.py
@@ -1,5 +1,6 @@
 import pygame
-
+import sys
+from pygame.locals import *
 #add variable named BLUE and set to color used for background
 BLUE = (81, 95, 255)
 
@@ -17,6 +18,33 @@ clock = pygame.time.Clock()
 
 play = True
 
+
+
+class Player:
+	def __init__(self, image, speed, posx, posy, width, height):
+
+		self.image = image
+		self.speed = speed
+		self.posx = posx
+		self.posy = posy
+		self.width = width
+		self.height = height
+
+
+	def update_rect(self, newx, newy):
+		self.rect = Rect(newx, newy, width, height)
+
+	def update_image(self, new_image):
+		self.image = new_image
+
+	def update_position(self, speed):
+		self.posx += speed
+		self.rect = self.rect = Rect(self.posx, self.posy + 200, 10, 1)
+
+
+player = Player(snowman, 7, 200, 300, 200, 200)
+
+
 while play:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -25,7 +53,7 @@ while play:
     game_display.fill(BLUE)
 
     #display image at the bottom in the middle of the screen
-    game_display.blit(snowman, (250,300))
+    game_display.blit(player.image, (player.posx, player.posy))
 
     pygame.display.update()
     clock.tick(fps)
